c9bb2395a02908a62f4f196c2803a6a0d810bf98
davi.ramon <davi.ramon@softwar.com.br>
2021-04-05 18:31:40 -0300

    att login admin

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,7 +17,14 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
+        // se esta autenticado e logado como admin
         if (Auth::guard($guard)->check()) {
+            if($guard == 'admin')
+            {       
+                return redirect()->route('admin.dashboard');
+            }
+            
+        // caso não esteja logado como admin retorna a view home do usuário
             return redirect('/home');
         }
 
