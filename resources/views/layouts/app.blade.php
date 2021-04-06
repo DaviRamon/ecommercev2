b1c588ec19c05294e00a90fb74acfdebb3708688
davi.ramon <davi.ramon@softwar.com.br>
2021-04-06 19:07:06 -0300

    att

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,13 +39,8 @@
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Entrar') }}</a>
+                                <a class="nav-link" href="{{ route('admin.login') }}">{{ __('Admin') }}</a>
                             </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Registro') }}</a>
-                                </li>
-                            @endif
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
